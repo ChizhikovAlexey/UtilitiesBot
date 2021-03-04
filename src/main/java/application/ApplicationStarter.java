f81c45fbc1cded49c98055f3e1a0389a07ba0df0
AlexeyChizhikov <chizhikov.alexey@icloud.com>
2021-03-04 20:43:54 +0300

    + dotenv

--- src/main/java/application/ApplicationStarter.java
+++ src/main/java/application/ApplicationStarter.java
@@ -8,10 +8,14 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.File;
+
 public class ApplicationStarter {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+        if (new File(".env").isFile()) {
+            Dotenv dotenv = Dotenv.load();
+            dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+        }
         System.getenv().forEach(System::setProperty);
         GenericApplicationContext ctx = new AnnotationConfigApplicationContext(Config.class);
         TelegramBot telegramBot = (TelegramBot) ctx.getBean("TelegramBot");
